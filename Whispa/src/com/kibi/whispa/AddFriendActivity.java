d9eb889678a5d4bf83742f43a96593546a641616
brutus-paul <brutus.paul.matshubeni@gmail.com>
2014-03-08 16:22:00 +0800

    Update AddFriendActivity, button still inactive

--- Whispa/src/com/kibi/whispa/AddFriendActivity.java
+++ Whispa/src/com/kibi/whispa/AddFriendActivity.java
@@ -1,8 +1,27 @@
 package com.kibi.whispa;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddFriendActivity extends Activity
 {
-
+	public Button add_button;
+	public EditText name ;
+	public EditText number ;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_friend);
+		
+	
+		name = (EditText)findViewById(R.id.editText1);
+		number = (EditText)findViewById(R.id.editText2);
+		add_button = (Button)findViewById(R.id.button1);
+	
+		
+	}
+		
 }
